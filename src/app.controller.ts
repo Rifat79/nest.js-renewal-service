7c84d22d04a2c72d27994bc7bde7a418b92d7d06
Rifat79 <33195501+Rifat79@users.noreply.github.com>
2025-10-26 17:21:28 +0600

    feat(testing message pub): added new route for testing message publishing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,42 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  EventPublisherService,
+  NotificationPayload,
+} from './event-publisher/event-publisher.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly eventPublisher: EventPublisherService,
+  ) {}
 
   @Get('health')
   getHealth() {
     return this.appService.getHealth();
+  }
+
+  @Get('event')
+  async testEvent() {
+    const payload: NotificationPayload = {
+      id: crypto.randomUUID(),
+      source: 'dcb-renewal-service',
+      subscriptionId: 'sub_123456789',
+      merchantTransactionId: 'MTXN-20251026-0001',
+      keyword: 'MUSIC_PLUS',
+      msisdn: '8801712345678',
+      paymentProvider: 'ROBI',
+      eventType: 'RENEWAL_SUCCESS',
+      amount: 50,
+      currency: 'BDT',
+      billingCycleDays: 30,
+      metadata: { planName: 'Music+ Monthly Plan', autoRenew: true },
+      timestamp: Date.now(),
+    };
+
+    const res = await this.eventPublisher.sendNotification(payload);
+
+    return res;
   }
 }
